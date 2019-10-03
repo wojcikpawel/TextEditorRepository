8e79c42f681e2265d007c152f0f1b5dbead896e1
Paweł Wójcik <asss@gmail.com>
2019-10-03 16:55:06 +0200

    change info caption

--- NativeAppClient/NativeAppClient/Form1.Designer.cs
+++ NativeAppClient/NativeAppClient/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(492, 370);
+            this.ClientSize = new System.Drawing.Size(766, 440);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "NativeAppClient";
             this.ResumeLayout(false);
 
         }
