7ac0df7cb3096d9cac4ab364114937a4c2b285dd
wojcikpawel <wojcikpawel25@gmail.com>
2020-05-28 00:04:06 +0200

    kolejny-przyklad

---
+++ prototype-web-socket/example/server.js
@@ -0,0 +1,31 @@
+//importing http module
+const http = require('http');
+//importing ws module
+const websocket = require('ws');
+
+//creating a http server
+const server = http.createServer((req, res) => {
+    res.end("I am connected");
+});
+
+//creating websocket server
+const wss = new websocket.Server({ server });
+//calling a method 'on' which is available on websocket object
+wss.on('headers', (headers, req) => {
+    //logging the header
+   // console.log(headers);
+});
+
+
+//Event: 'connection'
+wss.on('connection', (ws, req) => {
+    ws.send('This is a message from server, connection is established');
+    //receive the message from client on Event: 'message'
+    ws.on('message', (msg) => {
+        console.log(msg);
+    });
+});
+
+
+//making it listen to port 8000
+server.listen(8000);
